db51793cfda3440c4695b5d8cb4375724e43f811
parkjungkwan <pakjkwan@gmail.com>
2020-01-10 17:59:41 +0900

    18:00

--- java/com/example/application/WebActivity.kt
+++ java/com/example/application/WebActivity.kt
@@ -1,12 +1,12 @@
 package com.example.application
 
 import android.graphics.Bitmap
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.os.ProxyFileDescriptorCallback
 import android.view.View
-import android.webkit.JavascriptInterface
-import android.webkit.WebView
-import android.webkit.WebViewClient
+import android.webkit.*
 import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_web.*
 
@@ -15,10 +15,20 @@ class WebActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_web)
-        webview.settings.javaScriptEnabled = true
-        webview.addJavascriptInterface(WebBridge(),"java")
-        webview.loadUrl("file:///android_asset/index.html")
-        webview.setWebViewClient(object: WebViewClient(){
+        webView.settings.javaScriptEnabled = true
+        webView.settings.setSupportZoom(false)
+        webView.webChromeClient = object : WebChromeClient(){
+            override fun onShowFileChooser(
+                webview : WebView,
+                filePathCallback: ValueCallback<Array<Uri>>,
+                fileChooserParams: WebChromeClient.FileChooserParams
+            ): Boolean{
+                return true
+            }
+        }
+        webView.addJavascriptInterface(WebBridge(),"java")
+        webView.loadUrl("file:///android_asset/index.html")
+        webView.setWebViewClient(object: WebViewClient(){
             override fun onPageStarted(view: WebView?, url: String?, favicon: Bitmap?) {
                 super.onPageStarted(view, url, favicon)
                 pg.visibility = View.VISIBLE
